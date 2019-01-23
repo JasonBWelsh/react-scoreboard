4eed4c78c4956596eb08aeca50b7fd3dbb15d321
Jason Welsh <jasonwelsh@C02WN1F3G8WN.local>
2019-01-23 16:09:50 -0500

    Initial commit. Scoreboard front end app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const Header = () => {
+  return (
+    <header className="App-header">
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 3</span>
+    </header>
+  );
+}
+
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">
+        Player Name
+      </span>
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">99</span>
+        <button className="counter-action increment"> + </button>
+      </div>
+    </div>
+  );
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <Header />
+       <Player />
+       <Player />
+       <Player />
       </div>
     );
   }
