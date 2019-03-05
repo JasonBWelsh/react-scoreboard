f473892f9b66b8988a53b4d36847a39ff0b7eb8a
Jason Welsh <jasonwelsh@C02WN1F3G8WN.local>
2019-03-05 14:33:05 -0500

    [react-scoreboard] defensive commit before state refactor

--- src/index.js
+++ src/index.js
@@ -4,30 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-//TODO: extract `players` into separate data file and import
-// const players = [
-//     {
-//       playerName: 'Arwen',
-//       score: 99,
-//       id: 1
-//     },
-//     {
-//       playerName: 'Moya',
-//       score: 98,
-//       id: 2
-//     },
-//     {
-//       playerName: 'Senor',
-//       score: 97,
-//       id: 3
-//     },
-//     {
-//       playerName: 'Lily',
-//       score: 96,
-//       id: 4
-//     }
-//   ];
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
