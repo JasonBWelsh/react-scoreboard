11f1ab270658140f99277b5480978ed6e1c94150
Jason Welsh <jasonbwelsh@gmail.com>
2019-04-11 19:38:53 -0400

    Elapsed time calculated and stored in state

--- src/components/StopWatch.js
+++ src/components/StopWatch.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class StopWatch extends Component {
 
   state = {
-    isRunning: false
+    isRunning: false,
+    elapsedTime: 0,
+    previousTime: 0
   }
 
   handleStopWatch = () => {
@@ -13,6 +15,25 @@ class StopWatch extends Component {
         isRunning: !prevState.isRunning
       };
     });
+    if (!this.state.isRunning) {
+      this.setState({ previousTime: Date.now()});
+    }
+  }
+
+  componentDidMount() {
+    console.log('DRD __ `componentDidMount`');
+    this.intervalID = setInterval(() => this.tick(), 100);
+  }
+
+  tick = () => {
+    console.log('DRD __  `tick`');
+    if (this.state.isRunning) {
+      const now = Date.now();
+      this.setState( prevState => ({
+        previousTime: now,
+        elapsedTime: prevState.elapsedTime + (now - this.state.previousTime)
+      }));
+    }
   }
 
   render() {
@@ -22,7 +43,7 @@ class StopWatch extends Component {
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
-        <span className="stopwatch-time">0</span>
+        <span className="stopwatch-time">{this.state.elapsedTime}</span>
         <button onClick={this.handleStopWatch}>{runningState}</button>
         <button>Reset</button>
       </div>
