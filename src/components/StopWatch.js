a25a426503075580c0919bd64a145307eba64f1f
Jason Welsh <jasonbwelsh@gmail.com>
2019-04-11 19:47:46 -0400

    full stopwatch functionality

--- src/components/StopWatch.js
+++ src/components/StopWatch.js
@@ -20,6 +20,11 @@ class StopWatch extends Component {
     }
   }
 
+  handleResetStopWatch = () => {
+    console.log('DRD __ `handleResetStopWatch`');
+    this.setState({ elapsedTime: 0 });
+  }
+
   componentDidMount() {
     console.log('DRD __ `componentDidMount`');
     this.intervalID = setInterval(() => this.tick(), 100);
@@ -37,15 +42,15 @@ class StopWatch extends Component {
   }
 
   render() {
-
     let runningState = this.state.isRunning ? 'Stop' : 'Start';
+    const seconds = Math.floor(this.state.elapsedTime / 1000);
 
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
-        <span className="stopwatch-time">{this.state.elapsedTime}</span>
+        <span className="stopwatch-time">{seconds}</span>
         <button onClick={this.handleStopWatch}>{runningState}</button>
-        <button>Reset</button>
+        <button onClick={this.handleResetStopWatch}>Reset</button>
       </div>
     );
   }
